4b1e70deff3e0d455b31db2387361e8b1607b54c
yasmenMohammed <yabodabash@gmail.com>
2021-07-07 08:01:25 +0200

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String text;
+  String choice1;
+  String choice2;
+
+  Story({String storyText, String choiceText, String anotherText}) {
+    text = storyText;
+    choice1 = choiceText;
+    choice2 = anotherText;
+  }
+}
